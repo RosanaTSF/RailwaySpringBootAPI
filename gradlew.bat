7733315e073d37830fb22f11046266f80aec2f8f
Rosana TSF <rosanatsf@gmail.com>
2025-03-18 19:58:35 -0300

    Melhorias na estrutura do gradlew.bat (p/ config. Java)

--- gradlew.bat
+++ gradlew.bat
@@ -1,92 +1,48 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
+@rem ========================================================================
+@rem Script de inicialização do Gradle para Windows
+@rem 
+@rem Este script é usado para configurar e iniciar o Gradle Wrapper no sistema
+@rem Windows. Ele verifica se o Java está configurado corretamente e executa o
+@rem Gradle Wrapper com as opções definidas.
+@rem ========================================================================
 
-@if "%DEBUG%"=="" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+@rem Verifica se JAVA_HOME está configurado corretamente
+@rem Caso não esteja, tenta localizar o Java no PATH do sistema.
+if not defined JAVA_HOME (
+    echo JAVA_HOME não está definido. Tentando localizar o Java no PATH do sistema...
+    
+    rem O comando "where" busca o executável "java" no PATH do sistema
+    where java > NUL 2>&1
+    
+    rem Se o comando "where" não encontrar o Java, irá retornar um erro
+    if %ERRORLEVEL% neq 0 (
+        echo ERROR: JAVA_HOME não está definido e o comando 'java' não foi encontrado no PATH.
+        echo Por favor, defina a variável JAVA_HOME para apontar para a instalação do Java.
+        exit /b 1
+    )
+)
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
-
+@rem Definir o diretório onde o script está localizado e configurar APP_HOME
 set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-@rem This is normally unused
-set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
-
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem Configuração de opções padrões para a JVM
+@rem Essas opções determinam a memória inicial e máxima alocada para a JVM.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
-
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute
-
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto execute
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
-
-:execute
-@rem Setup the command line
-
+@rem Definir o classpath para o Gradle Wrapper
+@rem O classpath especifica onde o Gradle Wrapper JAR está localizado, permitindo que o Java o execute.
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-
-@rem Execute Gradle
+@rem Inicia o Gradle Wrapper com a JVM e as opções configuradas
+@rem O comando executa o Gradle Wrapper usando o arquivo JAR definido no CLASSPATH.
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
-:end
-@rem End local scope for the variables with windows NT shell
-if %ERRORLEVEL% equ 0 goto mainEnd
+@rem Verifica se a execução do Gradle foi bem-sucedida
+if %ERRORLEVEL% neq 0 (
+    echo ERROR: A execução do Gradle falhou.
+    exit /b %ERRORLEVEL%
+)
 
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-set EXIT_CODE=%ERRORLEVEL%
-if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
-exit /b %EXIT_CODE%
-
-:mainEnd
-if "%OS%"=="Windows_NT" endlocal
-
-:omega
+echo Gradle iniciou com sucesso.
+exit /b 0
